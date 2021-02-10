0215c8243c7cf19cd9bd9e89d265555224e2dd5e
Saad-PC-Certificat <s.zerhouni@piman-group.fr>
2021-02-10 14:59:48 +0100

    change update repo

--- updater.js
+++ updater.js
@@ -203,7 +203,7 @@ exports.getUpdateInfo = getUpdateInfo = (showNoUpdates)  => {
     var body = JSON.stringify({ platform: 'desktop', os: 'windows'});
     const request = net.request({
         method: 'POST',
-        url: 'https://api.sand.private-discuss.com/v1.0/release/get' ,
+        url: 'https://api-v2.private-discuss.com/v1.0/release/get',
         protocol: 'https:',
     });
     request.on('response', (response) => {
