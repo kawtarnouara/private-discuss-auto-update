cc826f2f02453d587e9c29c7ed530c69fca017db
kawtar <kawtar.nouara@gmail.com>
2021-11-01 10:15:40 +0100

    open new window

--- windows.js
+++ windows.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, Menu, session, ipcMain } = require('electron');
+const {app, BrowserWindow, Menu, session, ipcMain, shell } = require('electron');
 const ProgressBar = require('electron-progressbar');
 const { downloadManager } = require('./download');
 const {getUpdateInfo } = require('./updater');
