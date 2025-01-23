c20f27d85d2d7931ad0710239bf47a729f7f120e
mac <k.nouara@piman-group.fr>
2025-01-23 17:02:31 +0100

    generate mas app for ocp

--- notarize.js
+++ notarize.js
@@ -10,7 +10,7 @@ module.exports = async function (params) {
     console.log('afterSign hook triggered', params);
 
     // Same appId in electron-builder.
-    let appId = 'com.private.dicuss'
+    let appId = 'com.discuss.flows'
 
     let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
     if (!fs.existsSync(appPath)) {
